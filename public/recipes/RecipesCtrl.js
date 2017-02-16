26819e905c59fc48b7a93c03458326c1410fe763
Christa <christa.i.gorski@gmail.com>
2017-02-16 14:42:36 -0800

    Stores and retrieves data from $firebase database

--- public/recipes/RecipesCtrl.js
+++ public/recipes/RecipesCtrl.js
@@ -2,30 +2,54 @@ angular
 	.module('recipebox')
 	.controller('RecipesCtrl', RecipesCtrl);
 
-RecipesCtrl.$inject = ['$scope', '$location', 'recipesFactory', '$state'];
+RecipesCtrl.$inject = ['$scope', '$location', '$state', '$firebaseArray'];
 
-function RecipesCtrl($scope, $location, recipesFactory, $state) {
+function RecipesCtrl($scope, $location, $state, $firebaseArray) {
 
-	$scope.recipes;
 	$scope.allRecipes;
 	$scope.query = '';
 	$scope.categories = ["All", "Breakfast", "Lunch", "Dinner", "Dessert"];
-	recipesFactory.getRecipes().then(function(response) {
-		$scope.recipes = response;
-		$scope.allRecipes = $scope.categories.reduce(function(acc, value) {
-			var obj = {"category":'', "recipes": ''};
-			obj.category = value;
-			obj.recipes = $scope.recipes.filter(function(item) {
-				if(item.type.indexOf(value) > -1) {
-					return item;
-				} else if (value === "All") {
-					return item;
-				};
-			});
-			acc.push(obj);
-			return acc;
-		}, []);
-	}, function errorResponse(response){
-		console.log(response.statusText);
+
+	var ref = firebase.database().ref('recipes/');
+	$scope.recipes = $firebaseArray(ref);
+	console.log($firebaseArray(ref));
+	$scope.recipes.$loaded().then(function(data) {
+		$scope.allRecipes = $scope.categories.reduce($scope.recipereduce, []);
+	}).catch(function(error) {
+		console.log("Error ", error);
 	});
+	$scope.recipereduce = function(acc, curr) {
+		var obj = {"category":'', "recipes": ''};
+		obj.category = curr;
+		obj.recipes = $scope.recipes.filter(function(item){
+			if(item.type.indexOf(curr) > -1) {
+				return item;
+			} else if (curr === "All") {
+				return item;
+			};
+		});
+		acc.push(obj);
+		return acc;
+	};
+	//$scope.allRecipes = $scope.categories.reduce(function(acc, value) {
+	//	var obj = {"category":'', "recipes": ''};
+	//	obj.category = value;
+	//	$scope.recipes.$loaded()
+	//		.then(function(data) {
+	//			console.log(data === $scope.recipes);
+	//			obj.recipes = $scope.recipes.filter(function(item) {
+	//				if(item.type.indexOf(value) > -1) {
+	//					return item;
+	//				} else if (value === "All") {
+	//					return item;
+	//				};
+	//			});
+	//		})
+	//		.catch(function(error) {
+	//			console.log("Error: ", error);
+	//		});
+//
+	//	acc.push(obj);
+	//	return acc;
+	//}, []);
 }
