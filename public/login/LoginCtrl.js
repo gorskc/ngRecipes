077073fbc44b5f6f8b7b0fcbda8ba64892482250
Christa <christa.i.gorski@gmail.com>
2017-02-16 14:35:44 -0800

    Added 'logout' function

--- public/login/LoginCtrl.js
+++ public/login/LoginCtrl.js
@@ -4,22 +4,18 @@ angular
 
 LoginCtrl.$inject = ['$scope', '$state', 'currentAuth', 'Auth'];
 function LoginCtrl($scope, $state, currentAuth, Auth) {
-
+  $scope.loginemail;
+  $scope.loginpassword;
+  Auth.$onAuthStateChanged(function(firebaseUser) {
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      $scope.displayemail = firebaseUser.email;
+      document.getElementById('display-userName').textContent = $scope.displayemail;
+    }
+  })
   $scope.signIn = function() {
-    //console.log(currentAuth);
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if ($scope.email.length < 4) {
-      alert('Please enter a valid email address.');
-      return;
-    }
-    if ($scope.password.length < 4) {
-      alert('Please enter a password.');
-      return;
-    }
-
-    Auth.$signInWithEmailAndPassword($scope.email, $scope.password).then(function(response) {
-      console.log(response.displayName);
+    console.log($scope.loginemail);
+    Auth.$signInWithEmailAndPassword($scope.loginemail, $scope.loginpassword).then(function(response) {
       $state.go('recipes');
     }).catch(function(error) {
       var errorCode = error.code;
@@ -32,27 +28,16 @@ function LoginCtrl($scope, $state, currentAuth, Auth) {
       console.log(error);
     });
   };
-  $scope.isSignedIn = function() {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        console.log(user);
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        document.getElementById('display-login').style.visibility = 'hidden';
-        $state.go('recipes');
-      } else {
-        $state.go('home');
-      }
-    })
-  };
+
   $scope.logOut = function() {
     Auth.$signOut().then(function(){
       console.log('Logging out');
+      document.getElementById('display-userName').style.display = 'none';
       $state.go('home');
     }).catch(function(error) {
       console.log("Error signing user out:", error);
     });
   };
+
 
 }
